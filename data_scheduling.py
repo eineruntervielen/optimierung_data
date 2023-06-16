2426848a371a0661abfc586e44c94f7f6e0a0daf
Erik Kostic <erik.kostic@optware.de>
2023-06-16 11:16:07 +0200

    scheduling problem

--- data_scheduling.py
+++ data_scheduling.py
@@ -12,13 +12,12 @@ MACHINES = [f"m_{n}" for n in range(NUM_MACHINES)]
 
 def gen_random_job_table() -> pd.DataFrame:
     data = []
-    for j in range(NUM_JOBS):
+    for _ in range(NUM_JOBS):
         times = [f"t_{random.randint(1, 15)}" for _ in range(NUM_MACHINES)]
         perm_machines = MACHINES.copy()
         random.shuffle(perm_machines)
         data.append(list(zip(perm_machines, times)))
-    df = pd.DataFrame(data=data, columns=TASKS, index=JOBS)
-    return df
+    return pd.DataFrame(data=data, columns=TASKS, index=JOBS)
 
 
 if __name__ == '__main__':
